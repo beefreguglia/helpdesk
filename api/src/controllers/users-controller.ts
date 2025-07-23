5847825a71fc0441951c1ca82989de36ed0644d6
Bernardo <beefreguglia@gmail.com>
2025-07-23 02:15:21 -0300

    feature: create put in users

--- api/src/controllers/users-controller.ts
+++ api/src/controllers/users-controller.ts
@@ -1,48 +1,87 @@
-import { prisma } from "@/database/prisma";
-import { AppError } from "@/utils/AppError";
-import { UserRole } from "@prisma/client";
-import { hash } from "bcrypt";
-import type { Request, Response } from "express";
-import { z } from "zod";
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/AppError';
+import { UserRole } from '@prisma/client';
+import { hash } from 'bcrypt';
+import type { Request, Response } from 'express';
+import { z } from 'zod';
 
 class UsersController {
-	async create(request: Request, response: Response) {
-		const bodySchema = z.object({
-			name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
-			email: z
-				.string()
-				.trim()
-				.email({ message: "E-mail inválido" })
-				.toLowerCase(),
-			password: z
-				.string()
-				.min(6, { message: "A senha deve ter pelo menos 6 dígitos" }),
-			role: z
-				.enum([UserRole.ADMIN, UserRole.CLIENT, UserRole.TECHNICIAN])
-				.default(UserRole.CLIENT),
-		});
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: 'Nome é obrigatório' }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: 'E-mail inválido' })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: 'A senha deve ter pelo menos 6 dígitos' }),
+      role: z
+        .enum([UserRole.ADMIN, UserRole.CLIENT, UserRole.TECHNICIAN])
+        .default(UserRole.CLIENT),
+    });
 
-		const { email, name, password, role } = bodySchema.parse(request.body);
+    const { email, name, password, role } = bodySchema.parse(request.body);
 
-		const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
 
-		if (userWithSameEmail) {
-			throw new AppError("Já existe um usuário cadastrado com esse e-mail");
-		}
+    if (userWithSameEmail) {
+      throw new AppError('Já existe um usuário cadastrado com esse e-mail');
+    }
 
-		const hashedPassword = await hash(password, 8);
+    const hashedPassword = await hash(password, 8);
 
-		await prisma.user.create({
-			data: {
-				email,
-				name,
-				password: hashedPassword,
-				role,
-			},
-		});
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        role,
+      },
+    });
 
-		response.status(201).json();
-	}
+    response.status(201).json();
+  }
+
+  async update(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const bodySchema = z.object({
+      name: z.string().trim().optional(),
+      email: z.string().optional(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const { email, name } = bodySchema.parse(request.body);
+
+    if (email) {
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (userWithSameEmail) {
+        throw new AppError('E-mail já utilizado!', 500);
+      }
+    }
+
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+
+    response.status(200).json();
+  }
 }
 
 export { UsersController };
--- api/src/routes/users-routes.ts
+++ api/src/routes/users-routes.ts
@@ -1,9 +1,10 @@
-import { UsersController } from "@/controllers/users-controller";
-import { Router } from "express";
+import { UsersController } from '@/controllers/users-controller';
+import { Router } from 'express';
 
 const usersRoutes = Router();
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post('/', usersController.create);
+usersRoutes.put('/:id', usersController.update);
 
 export { usersRoutes };
